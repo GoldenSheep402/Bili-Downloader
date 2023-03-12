2b3cabdc4b395b787c8e8b7768d314813f88fcab
GoldenSheep <805069152@qq.com>
2023-03-13 00:26:44 +0800

    Update merger.go

--- internal/merger/merger.go
+++ internal/merger/merger.go
@@ -1,14 +1,12 @@
 package merger
 
-import "C"
-
 import (
 	"fmt"
 	"os/exec"
 )
 
 func MergeVideoAndAudio(videoPath string, audioPath string, outputPath string) error {
-	cmd := exec.Command("ffmpeg", "-y", "-i", videoPath, "-i", audioPath, "-c", "copy", outputPath)
+	cmd := exec.Command("/lib/ffmpeg/bin/ffmpeg", "-y", "-i", videoPath, "-i", audioPath, "-c", "copy", outputPath)
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println("Failed to merge video and audio:", err)
